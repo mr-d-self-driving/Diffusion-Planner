8eb404065deda7aa055cdfbde5f53d953f744f26
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-25 22:36:36 +0900

    FIxed to valid
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- valid_run.sh
+++ valid_run.sh
@@ -10,7 +10,7 @@ TRAIN_SET_LIST_PATH="REPLACE_WITH_TRAIN_SET_LIST_PATH"
 MODEL_PATH="REPLACE_WITH_MODEL_PATH"
 ###################################
 
-python -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone train_predictor.py \
+python -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone valid_predictor.py \
 --train_set  $TRAIN_SET_PATH \
 --train_set_list  $TRAIN_SET_LIST_PATH \
 --resume_model_path $MODEL_PATH \
