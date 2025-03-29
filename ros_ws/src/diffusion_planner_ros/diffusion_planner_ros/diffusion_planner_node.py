d5e3c133300c3a316c41002eda6a1a5e221c5f08
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-29 13:25:49 +0900

    Added kinematic_state_callback
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/diffusion_planner_node.py
+++ ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/diffusion_planner_node.py
@@ -4,11 +4,26 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
+from nav_msgs.msg import Odometry
 
 
 class DiffusionPlannerNode(Node):
     def __init__(self):
         super().__init__("diffusion_planner_node")
+
+        self.kinematic_state_sub = self.create_subscription(
+            Odometry,
+            "/localization/kinematic_state",
+            self.cb_kinematic_state,
+            10,
+        )
+
+        self.get_logger().info("Diffusion Planner Node has been initialized")
+
+    def cb_kinematic_state(self, msg):
+        self.get_logger().info(
+            f"Received kinematic state. Position: [{msg.pose.pose.position.x}, {msg.pose.pose.position.y}, {msg.pose.pose.position.z}]"
+        )
 
 
 def main(args=None):
