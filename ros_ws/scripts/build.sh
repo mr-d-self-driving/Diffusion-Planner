6120db745826bb50d1ece5b8ce6b93c8b2be5334
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-31 16:07:22 +0900

    Added rosdep
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/build.sh
+++ ros_ws/scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd $(dirname $0)/../
+source /opt/ros/humble/setup.bash
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 colcon build \
   --symlink-install \
   --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF \
