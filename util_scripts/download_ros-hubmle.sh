9004438909a1ef53038715807568a316ae3007dd
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-31 18:55:42 +0900

    Added desktop
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- util_scripts/download_ros-hubmle.sh
+++ util_scripts/download_ros-hubmle.sh
@@ -8,6 +8,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y ros-humble-ros-base
+sudo apt install -y ros-humble-desktop ros-humble-ros-base
 sudo apt install -y python3-colcon-common-extensions
 sudo apt install -y python3-rosdep
