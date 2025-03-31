69ebc5a963d372ab42083b4f5177954b92a04534
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-31 16:51:56 +0900

    Fixed run.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/run.sh
+++ ros_ws/scripts/run.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)/../
 source /opt/ros/humble/setup.bash
 source ./install/setup.bash
-ros2 run diffusion_planner_ros diffusion_planner_node
+ros2 run diffusion_planner_ros diffusion_planner_node --ros-args -p vector_map_path:=/home/shintarosakoda/data/misc/20250329_psim_rosbag/map/lanelet2_map.osm
