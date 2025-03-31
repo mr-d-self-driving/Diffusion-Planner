1a445a0ced6d1a0a5df78793d6ae17a5ef9d6098
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-03-31 17:22:12 +0900

    Added play.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ ros_ws/scripts/play.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd $(dirname $0)/../
+source /opt/ros/humble/setup.bash
+source ./install/setup.bash
+ros2 bag play /home/shintarosakoda/data/misc/20250329_psim_rosbag/rosbag2_2025_03_29-13_12_14 -r 3.0
