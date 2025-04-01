07cb8d9b845ee8e53c45a17cb00a68b66bab1a39
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-01 15:16:04 +0900

    Fixed rate
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/play.sh
+++ ros_ws/scripts/play.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rate=${1:-2.0}
+
 cd $(dirname $0)/../
 source /opt/ros/humble/setup.bash
 source ./install/setup.bash
-ros2 bag play /home/shintarosakoda/data/misc/20250329_psim_rosbag/rosbag2_2025_03_29-13_12_14 -r 3.0
+ros2 bag play /home/shintarosakoda/data/misc/20250329_psim_rosbag/rosbag2_2025_03_29-13_12_14 -r $rate
