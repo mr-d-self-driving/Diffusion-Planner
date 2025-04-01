dc75978b21e1abb92d96e24eb064c39a8c7cd411
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-01 09:12:07 +0900

    Added rotation
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/src/diffusion_planner_ros/test/test_input_vector_map.py
+++ ros_ws/src/diffusion_planner_ros/test/test_input_vector_map.py
@@ -5,6 +5,8 @@ import lanelet2
 import argparse
 from diffusion_planner_ros.lanelet2_utils.lanelet_converter import convert_lanelet
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation
+import numpy as np
 
 
 def parse_args():
@@ -34,26 +36,48 @@ if __name__ == "__main__":
 
     ego_x = 3734.4
     ego_y = 73680.015625
-    WIDTH = 50
+    ego_z = 19.519
+    ego_qx = 0.0005874986553164094
+    ego_qy = -0.0022261576737295824
+    ego_qz = 0.2551699815886937
+    ego_qw = 0.9668934685700217
+    WIDTH = 100
+
+    # 自車中心に座標変換するための行列を作成
+    rot = Rotation.from_quat([ego_qx, ego_qy, ego_qz, ego_qw])
+    translation = np.array([ego_x, ego_y, ego_z])
+    transform_matrix = rot.as_matrix()
+    transform_matrix_4x4 = np.eye(4)
+    transform_matrix_4x4[:3, :3] = transform_matrix
+    transform_matrix_4x4[:3, 3] = translation
+    inv_transform_matrix_4x4 = np.eye(4)
+    inv_transform_matrix_4x4[:3, :3] = transform_matrix.T
+    inv_transform_matrix_4x4[:3, 3] = -transform_matrix.T @ translation
 
     # Plot the map
     plt.figure()
     for segment_id, segment in result.lane_segments.items():
         waypoints = segment.polyline.waypoints
         print(f"{waypoints.shape=}")  # (N, 3)
+        # 自車座標系に変換
+        waypoints_4xN = np.vstack((waypoints.T, np.ones(waypoints.shape[0])))
+        waypoints_ego = inv_transform_matrix_4x4 @ waypoints_4xN
+        waypoints = waypoints_ego[:3, :].T
+
         # x, yがegoからWIDTH内のものだけを抽出
         mask = (
-            (waypoints[:, 0] > ego_x - WIDTH)
-            & (waypoints[:, 0] < ego_x + WIDTH)
-            & (waypoints[:, 1] > ego_y - WIDTH)
-            & (waypoints[:, 1] < ego_y + WIDTH)
+            (waypoints[:, 0] > -WIDTH)
+            & (waypoints[:, 0] < WIDTH)
+            & (waypoints[:, 1] > -WIDTH)
+            & (waypoints[:, 1] < WIDTH)
         )
         filtered_waypoints = waypoints[mask]
         plt.plot(filtered_waypoints[:, 0], filtered_waypoints[:, 1], "r-")
 
     plt.xlabel("x[m]")
     plt.ylabel("y[m]")
-    plt.axis("equal")
+    plt.xlim(-WIDTH, WIDTH)
+    plt.ylim(-WIDTH, WIDTH)
     save_path = "./test_input_vector_map.png"
     plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05)
     print(f"Saved plot to {save_path}")
