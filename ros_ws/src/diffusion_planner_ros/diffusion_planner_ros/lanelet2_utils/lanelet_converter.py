4f976bb4d24867922139f9a3c9caaa33d5a903fa
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-01 14:53:23 +0900

    Added diff
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/lanelet_converter.py
+++ ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/lanelet_converter.py
@@ -504,9 +504,13 @@ def process_segment(segment, inv_transform_matrix_4x4, mask_range):
     left_boundaries = resample_waypoints(left_boundaries, 20)
     right_boundaries = resample_waypoints(right_boundaries, 20)
 
+    diff_centerlines = filtered_centerlines[1:] - filtered_centerlines[:-1]
+    diff_centerlines = np.insert(diff_centerlines, diff_centerlines.shape[0], 0, axis=0)
+
     curr_data = np.concatenate(
         (
             filtered_centerlines[:, 0:2],  # xyのみ
+            diff_centerlines[:, 0:2],  # xyのみ
             left_boundaries[:, 0:2],  # xyのみ
             right_boundaries[:, 0:2],  # xyのみ
         ),
