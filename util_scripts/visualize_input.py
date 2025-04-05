123fc3b0e07cceebec44d5136128cec2c5301f39
Shintaro Sakoda <sakoda@keio.jp>
2025-04-05 11:26:26 +0900

    Added num
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- util_scripts/visualize_input.py
+++ util_scripts/visualize_input.py
@@ -4,12 +4,14 @@ from pathlib import Path
 from diffusion_planner.utils.visualize_input import visualize_inputs
 from diffusion_planner.utils.config import Config
 from copy import deepcopy
+from shutil import rmtree
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_pkl", type=Path)
     parser.add_argument("args_json", type=Path)
+    parser.add_argument("--num", type=int, default=1)
     return parser.parse_args()
 
 
@@ -17,6 +19,7 @@ if __name__ == "__main__":
     args = parse_args()
     input_pkl = args.input_pkl
     args_json = args.args_json
+    num = args.num
 
     with open(input_pkl, "rb") as f:
         data = pickle.load(f)
@@ -30,12 +33,12 @@ if __name__ == "__main__":
         assert bs == value.shape[0], "Batch size mismatch"
 
     save_dir = Path("./visualize")
+    rmtree(save_dir, ignore_errors=True)
     save_dir.mkdir(exist_ok=True)
-    for b in range(bs):
+    for b in range(min(bs, num)):
         inputs = deepcopy(data)
         for key, value in inputs.items():
             inputs[key] = value[b : b + 1]
         save_path = save_dir / f"input_{b:08d}.png"
         visualize_inputs(inputs, config_obj.observation_normalizer, save_path)
         print(f"Saved to {save_path}")
-        break
