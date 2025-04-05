f2f9745cb25ab454998e9136d022d16b3e0dd70e
Shintaro Sakoda <sakoda@keio.jp>
2025-04-05 11:12:21 +0900

    Updated
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- util_scripts/visualize_input.py
+++ util_scripts/visualize_input.py
@@ -3,6 +3,7 @@ import argparse
 from pathlib import Path
 from diffusion_planner.utils.visualize_input import visualize_inputs
 from diffusion_planner.utils.config import Config
+from copy import deepcopy
 
 
 def parse_args():
@@ -22,9 +23,19 @@ if __name__ == "__main__":
 
     config_obj = Config(args_json)
 
+    bs = -1
     for key, value in data.items():
         print(f"{key}\t{value.shape}")
+        bs = value.shape[0] if bs == -1 else bs
+        assert bs == value.shape[0], "Batch size mismatch"
 
-    save_path = "./input.png"
-    visualize_inputs(data, config_obj.observation_normalizer, save_path)
-    print(f"Saved to {save_path}")
+    save_dir = Path("./visualize")
+    save_dir.mkdir(exist_ok=True)
+    for b in range(bs):
+        inputs = deepcopy(data)
+        for key, value in inputs.items():
+            inputs[key] = value[b : b + 1]
+        save_path = save_dir / f"input_{b:08d}.png"
+        visualize_inputs(inputs, config_obj.observation_normalizer, save_path)
+        print(f"Saved to {save_path}")
+        break
