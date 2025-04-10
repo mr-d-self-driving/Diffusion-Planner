3f681bc9eb95741f82234feb4f5e821e034f3249
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-10 11:04:22 +0900

    Added check_npz.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ util_scripts/check_npz.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pathlib import Path
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("npz_path", type=Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    npz_path = args.npz_path
+
+    # Load the npz file
+    data = np.load(npz_path, allow_pickle=True)
+
+    # Print the shape of each array
+    for key in data.keys():
+        val = data[key]
+        print(f"{key}\t{val.dtype}\t{val.shape}")
