cbab15a88fb8b8fec96961a5a0900596545ffca6
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-10 21:24:47 +0900

    Added use_sim_time
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/run.sh
+++ ros_ws/scripts/run.sh
@@ -6,4 +6,5 @@ source ./install/setup.bash
 ros2 run diffusion_planner_ros diffusion_planner_node --ros-args \
     -p vector_map_path:=/home/shintarosakoda/data/driving_data/map/lanelet2_map.osm \
     -p config_json_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/args.json \
-    -p ckpt_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/latest.pth
+    -p ckpt_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/latest.pth \
+    -p use_sim_time:=true \
