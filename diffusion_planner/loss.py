d2bb7fed6be8a881e045f5abf5bcaca303dcc7d5
Shintaro Sakoda <sakoda@keio.jp>
2025-04-12 18:52:57 +0900

    Added flow_matching_loss_func
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- diffusion_planner/loss.py
+++ diffusion_planner/loss.py
@@ -68,3 +68,64 @@ def diffusion_loss_func(
     assert not torch.isnan(dpm_loss).sum(), f"loss cannot be nan, z={z}"
 
     return loss, decoder_output
+
+
+def flow_matching_loss_func(
+    model: nn.Module,
+    inputs: Dict[str, torch.Tensor],
+    marginal_prob: Callable[[torch.Tensor], torch.Tensor],
+
+    futures: Tuple[torch.Tensor, torch.Tensor],
+    
+    norm: StateNormalizer,
+    loss: Dict[str, Any],
+
+    model_type: str,
+    eps: float = 1e-3,
+):   
+    ego_future, neighbors_future, neighbor_future_mask = futures
+    neighbors_future_valid = ~neighbor_future_mask # [B, P, V]
+
+    B, Pn, T, _ = neighbors_future.shape
+    ego_current, neighbors_current = inputs["ego_current_state"][:, :4], inputs["neighbor_agents_past"][:, :Pn, -1, :4]
+    neighbor_current_mask = torch.sum(torch.ne(neighbors_current[..., :4], 0), dim=-1) == 0
+    neighbor_mask = torch.concat((neighbor_current_mask.unsqueeze(-1), neighbor_future_mask), dim=-1)
+
+    gt_future = torch.cat([ego_future[:, None, :, :], neighbors_future[..., :]], dim=1) # [B, P = 1 + 1 + neighbor, T, 4]
+    current_states = torch.cat([ego_current[:, None], neighbors_current], dim=1) # [B, P, 4]
+
+    t = torch.rand(B, device=gt_future.device) * (1 - eps) + eps # [B,]
+    z = torch.randn_like(gt_future, device=gt_future.device) # [B, P, T, 4]
+
+    all_gt = torch.cat([current_states[:, :, None, :], norm(gt_future)], dim=2)  # [B, P, 1 + T, 4]
+    all_gt[:, 1:][neighbor_mask] = 0.0
+
+    # t=0 is noise, t=1 is data
+    xT = (1-t) * z + t * all_gt[:, :, 1:, :] # [B, P, T, 4]
+    xT = torch.cat([all_gt[:, :, :1, :], xT], dim=2)  # [B, P, 1 + T, 4]
+
+    merged_inputs = {
+        **inputs,
+        "sampled_trajectories": xT,
+        "diffusion_time": t,
+    }
+
+    _, decoder_output = model(merged_inputs) # [B, P, 1 + T, 4]
+    v = decoder_output["score"][:, :, 1:, :] # [B, P, T, 4]
+
+    target  = (all_gt[:, :, 1:, :] - z)
+
+    dpm_loss = torch.sum((v - target)**2, dim=-1)
+    
+    masked_prediction_loss = dpm_loss[:, 1:, :][neighbors_future_valid]
+
+    if masked_prediction_loss.numel() > 0:
+        loss["neighbor_prediction_loss"] = masked_prediction_loss.mean()
+    else:
+        loss["neighbor_prediction_loss"] = torch.tensor(0.0, device=masked_prediction_loss.device)
+
+    loss["ego_planning_loss"] = dpm_loss[:, 0, :].mean()
+
+    assert not torch.isnan(dpm_loss).sum(), f"loss cannot be nan, z={z}"
+
+    return loss, decoder_output
