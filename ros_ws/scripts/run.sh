43b12f769e64474bb4733db21ea30516c3288cf4
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-20 16:50:17 +0900

    Fixed path
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/run.sh
+++ ros_ws/scripts/run.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/../
 source /opt/ros/humble/setup.bash
 source ./install/setup.bash
 ros2 run diffusion_planner_ros diffusion_planner_node --ros-args \
-    -p vector_map_path:=/home/shintarosakoda/data/driving_data/map/lanelet2_map.osm \
+    -p vector_map_path:=/home/shintarosakoda/data/driving_data/map_for_20250325/lanelet2_map.osm \
     -p config_json_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/args.json \
     -p ckpt_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/latest.pth \
     -p use_sim_time:=true \
