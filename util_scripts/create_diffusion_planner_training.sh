c365e1838d973f0590f33ac0c7c94ea3d410f56f
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-21 09:08:26 +0900

    Fixed comments
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- util_scripts/create_diffusion_planner_training.sh
+++ util_scripts/create_diffusion_planner_training.sh
@@ -11,11 +11,11 @@ set -eu
 # ...
 # ->
 # [
-#     "us-nv-las-vegas-strip_150e044bab0a57a8.npz",
-#     "us-nv-las-vegas-strip_2324ed8cd5a75cb9.npz",
-#     "us-nv-las-vegas-strip_47ceedefd88158a3.npz",
-#     "us-nv-las-vegas-strip_4e507af0fcf75ad3.npz",
-#     "us-nv-las-vegas-strip_60b40cbd09e85a06.npz",
+#     "/abs_path/to/us-nv-las-vegas-strip_150e044bab0a57a8.npz",
+#     "/abs_path/to/us-nv-las-vegas-strip_2324ed8cd5a75cb9.npz",
+#     "/abs_path/to/us-nv-las-vegas-strip_47ceedefd88158a3.npz",
+#     "/abs_path/to/us-nv-las-vegas-strip_4e507af0fcf75ad3.npz",
+#     "/abs_path/to/us-nv-las-vegas-strip_60b40cbd09e85a06.npz",
 #     ...
 # ]
 
