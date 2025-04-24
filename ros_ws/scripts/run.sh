c6eb5be790def1a95d9801db27efbaa5a89a25fa
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-24 11:01:04 +0900

    Fixed run.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/run.sh
+++ ros_ws/scripts/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -eux
 
+model_dir=$(readlink -f $1)
 cd $(dirname $0)/../
+
+set +eux
 source /opt/ros/humble/setup.bash
 source ./install/setup.bash
+set -eux
+
 ros2 run diffusion_planner_ros diffusion_planner_node --ros-args \
-    -p vector_map_path:=/home/shintarosakoda/data/driving_data/map_for_20250325/lanelet2_map.osm \
-    -p config_json_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/args.json \
-    -p ckpt_path:=/media/shintarosakoda/5EA85517A854EF51/diffusion_planner_training_result/train_result/2025-03-20-180651_datasize_1M/latest.pth \
-    -p use_sim_time:=true \
+    -p vector_map_path:=$HOME/data/nas_copy/tieriv_dataset/driving_dataset/map/2025-04-16/lanelet2_map.osm \
+    -p config_json_path:=$model_dir/args.json \
+    -p ckpt_path:=$model_dir/latest.pth \
+    -p onnx_path:=$model_dir/model.onnx \
+    -p backend:=ONNXRUNTIME \
     -p batch_size:=1 \
-    -p backend:=PYTHORCH \
-    -p onnx_path:=/home/danielsanchez/Diffusion-Planner/ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/conversion/model.onnx
+    -p use_sim_time:=true \
