2bc79eddafbb36b8dfb4d4936b8d42e86308fc6f
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-28 13:37:21 +0900

    Removed AGENT_TYPE_MAPPING
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/constant.py
+++ ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/constant.py
@@ -4,56 +4,6 @@ from .agents_base import AgentType
 from .map import MapType
 from .uuid import uuid
 
-AGENT_TYPE_MAPPING: dict[str, AgentType] = {
-    # Vehicle
-    "vehicle.car": AgentType.VEHICLE,
-    "vehicle.emergency (ambulance & police)": AgentType.VEHICLE,
-    "vehicle": AgentType.VEHICLE,
-    "car": AgentType.VEHICLE,
-    "police_car": AgentType.VEHICLE,
-    "ambulance": AgentType.VEHICLE,
-    # Pedestrian
-    "pedestrian.adult": AgentType.PEDESTRIAN,
-    "pedestrian.child": AgentType.PEDESTRIAN,
-    "pedestrian.police_officer": AgentType.PEDESTRIAN,
-    "pedestrian.stroller": AgentType.PEDESTRIAN,
-    "pedestrian.personal_mobility": AgentType.PEDESTRIAN,
-    "pedestrian.construction_worker": AgentType.PEDESTRIAN,
-    "pedestrian.wheelchair": AgentType.PEDESTRIAN,
-    "pedestrian": AgentType.PEDESTRIAN,
-    "police_officer": AgentType.PEDESTRIAN,
-    "stroller": AgentType.PEDESTRIAN,
-    "personal_mobility": AgentType.PEDESTRIAN,
-    "construction_worker": AgentType.PEDESTRIAN,
-    "wheelchair": AgentType.PEDESTRIAN,
-    # Cyclist
-    "vehicle.bicycle": AgentType.CYCLIST,
-    "bicycle": AgentType.CYCLIST,
-    # Motorcyclist
-    "vehicle.motorbike": AgentType.MOTORCYCLIST,
-    "vehicle.motorcycle": AgentType.MOTORCYCLIST,
-    "motorbike": AgentType.MOTORCYCLIST,
-    "motorcycle": AgentType.MOTORCYCLIST,
-    # Large vehicle
-    "vehicle.bus": AgentType.LARGE_VEHICLE,
-    "vehicle.bus (bendy & rigid)": AgentType.LARGE_VEHICLE,
-    "vehicle.trailer": AgentType.LARGE_VEHICLE,
-    "vehicle.truck": AgentType.LARGE_VEHICLE,
-    "vehicle.construction": AgentType.VEHICLE,
-    "trailer": AgentType.LARGE_VEHICLE,
-    "bus": AgentType.LARGE_VEHICLE,
-    "truck": AgentType.LARGE_VEHICLE,
-    "fire_truck": AgentType.LARGE_VEHICLE,
-    "semi_trailer": AgentType.LARGE_VEHICLE,
-    "tractor_unit": AgentType.LARGE_VEHICLE,
-    "construction": AgentType.LARGE_VEHICLE,
-    # Static
-    "movable_object.trafficcone": AgentType.STATIC,
-    "movable_object.traffic_cone": AgentType.STATIC,
-    "movable_object.pushable_pullable": AgentType.STATIC,
-    "static_object.bicycle_rack": AgentType.STATIC,
-}
-
 MAP_TYPE_MAPPING: dict[str, MapType] = {
     "road": MapType.ROADWAY,
     "highway": MapType.ROADWAY,
