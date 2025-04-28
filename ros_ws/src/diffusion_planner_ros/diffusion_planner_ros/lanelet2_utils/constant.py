5b28161bea5955f6a39b20cc6b1b3f3b793f950b
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-04-28 13:39:34 +0900

    Removed MAP_TYPE_COLORS
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/constant.py
+++ ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/lanelet2_utils/constant.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from .map import MapType
-from .uuid import uuid
 
 MAP_TYPE_MAPPING: dict[str, MapType] = {
     "road": MapType.ROADWAY,
@@ -17,20 +16,6 @@ MAP_TYPE_MAPPING: dict[str, MapType] = {
     "unknown": MapType.UNKNOWN,
 }
 
-MAP_TYPE_COLORS: dict[MapType, list[float]] = {
-    MapType.ROADWAY: [0.1, 0.9, 0.0, 0.99],
-    MapType.BIKE_LANE: [0.0, 0.3, 0.0, 0.99],
-    MapType.DASHED: [0.9, 0.9, 0.0, 0.99],
-    MapType.SOLID: [0.0, 0.9, 0.9, 0.99],
-    MapType.SOLID_DASH: [0.6, 0.6, 0.1, 0.99],
-    MapType.DOUBLE_DASH: [0.0, 0.0, 0.3, 0.99],
-    MapType.UNKNOWN: [0.99, 0.0, 0.0, 0.99],
-    MapType.CROSSWALK: [0.0, 0.0, 0.99, 0.99],
-    MapType.BUS_LANE: [0.4, 0.4, 0.4, 0.99],
-}
-
 T4_LANE: tuple[str, ...] = ("road", "highway", "road_shoulder", "bicycle_lane")
 T4_ROADLINE: tuple[str, ...] = ("dashed", "solid", "dashed_dashed", "virtual")
 T4_ROADEDGE: tuple[str, ...] = ("road_border",)
-
-EGO_ID = uuid("AV")
