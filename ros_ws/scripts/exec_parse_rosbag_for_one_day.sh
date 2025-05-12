2f43ff3a82c0d5aa25a1613e6f16f3105f9dba58
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-05-12 19:28:46 +0900

    Fixed to use date/time map
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_ws/scripts/exec_parse_rosbag_for_one_day.sh
+++ ros_ws/scripts/exec_parse_rosbag_for_one_day.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 bag_dir_list=$(find $target_dir -mindepth 1 -maxdepth 1 -type d | sort)
 
 date=$(basename $target_dir)
-save_root=$target_dir/../../../../private_workspace/diffusion_planner/preprocessed_for_diffusion_planner6/$date
+save_root=$target_dir/../../../../private_workspace/diffusion_planner/preprocessed_for_diffusion_planner7/$date
 mkdir -p $save_root
 save_root=$(readlink -f $save_root)
 echo $save_root
@@ -20,6 +20,11 @@ for bag_dir in $bag_dir_list; do
     # I assume that the map is consistent for the same day
     map_dir=$bag_dir/../../../map/$date
     map_path=$map_dir/lanelet2_map.osm
+
+    # if there is map/$date/$time, use it
+    if [ -d $map_dir/$time ]; then
+        map_path=$map_dir/$time/lanelet2_map.osm
+    fi
 
     # out_dir=$save_root/10-10-58
     out_dir=$save_root/$time
