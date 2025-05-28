67ffe782bcbed33cdfef95af1ec522d349742c0f
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-05-28 10:27:32 +0900

    Revert "Removed lost time"
    This reverts commit d24e2cf8a07c593e2bcc106364e367101399f6d7.

--- ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/utils.py
+++ ros_ws/src/diffusion_planner_ros/diffusion_planner_ros/utils.py
@@ -19,6 +19,7 @@ class TrackingObject:
     kinematics_list: list
     shape_list: list
     class_label: int
+    lost_time: int
 
 
 def parse_timestamp(stamp) -> int:
@@ -101,6 +102,35 @@ def rot3x3_to_heading_cos_sin(rot3x3):
     return cos_heading, sin_heading
 
 
+def _forward_kinematics(kinematics: TrackedObjectKinematics, sec: float):
+    """
+    Forward kinematics for a tracked object
+    """
+    pose = kinematics.pose_with_covariance.pose
+    twist = kinematics.twist_with_covariance.twist
+    new_kinematics = deepcopy(kinematics)
+    pose_in_map_4x4 = pose_to_mat4x4(pose)
+    twist_linear_in_local = np.array([twist.linear.x, twist.linear.y, twist.linear.z])
+    twist_angular = Rotation.from_euler(
+        "xyz", [twist.angular.x * sec, twist.angular.y * sec, twist.angular.z * sec], degrees=False
+    )
+    twist_in_map = pose_in_map_4x4[0:3, 0:3] @ twist_linear_in_local
+
+    # Update position
+    new_pose = pose_in_map_4x4.copy()
+    new_pose[0:3, 3] += twist_in_map * sec
+    new_pose[0:3, 0:3] = (twist_angular.as_matrix()) @ pose_in_map_4x4[0:3, 0:3]
+    new_kinematics.pose_with_covariance.pose.position.x = new_pose[0, 3]
+    new_kinematics.pose_with_covariance.pose.position.y = new_pose[1, 3]
+    new_kinematics.pose_with_covariance.pose.position.z = new_pose[2, 3]
+    quat = Rotation.from_matrix(new_pose[0:3, 0:3]).as_quat()
+    new_kinematics.pose_with_covariance.pose.orientation.x = quat[0]
+    new_kinematics.pose_with_covariance.pose.orientation.y = quat[1]
+    new_kinematics.pose_with_covariance.pose.orientation.z = quat[2]
+    new_kinematics.pose_with_covariance.pose.orientation.w = quat[3]
+    return new_kinematics
+
+
 def create_current_ego_state(kinematic_state_msg, acceleration_msg, wheel_base):
     ego_twist_linear = kinematic_state_msg.twist.twist.linear
     ego_twist_angular = kinematic_state_msg.twist.twist.angular
@@ -130,8 +160,10 @@ def create_current_ego_state(kinematic_state_msg, acceleration_msg, wheel_base):
     return ego_current_state
 
 
-def tracking_one_step(msg: TrackedObjects, tracked_objs: dict) -> dict:
-    updated_tracked_objs = {}
+def tracking_one_step(msg: TrackedObjects, tracked_objs: dict, lost_time_limit: int = 10) -> dict:
+    updated_tracked_objs = tracked_objs.copy()
+    for key in updated_tracked_objs:
+        updated_tracked_objs[key].lost_time += 1
     label_map = {
         0: 0,  # unknown -> vehicle
         1: 0,  # car -> vehicle
@@ -157,12 +189,23 @@ def tracking_one_step(msg: TrackedObjects, tracked_objs: dict) -> dict:
             tracked_obj = tracked_objs[object_id_bytes]
             tracked_obj.shape_list.append(shape)
             tracked_obj.kinematics_list.append(kinematics)
+            tracked_obj.lost_time = 0
             updated_tracked_objs[object_id_bytes] = tracked_obj
         else:
             updated_tracked_objs[object_id_bytes] = TrackingObject(
                 kinematics_list=[kinematics],
                 shape_list=[shape],
                 class_label=label_in_model,
+                lost_time=0,
+            )
+
+    for key in list(updated_tracked_objs.keys()):
+        if updated_tracked_objs[key].lost_time > lost_time_limit:
+            del updated_tracked_objs[key]
+        elif updated_tracked_objs[key].lost_time > 0:
+            updated_tracked_objs[key].shape_list.append(updated_tracked_objs[key].shape_list[-1])
+            updated_tracked_objs[key].kinematics_list.append(
+                _forward_kinematics(updated_tracked_objs[key].kinematics_list[-1], sec=0.1)
             )
 
     return updated_tracked_objs
