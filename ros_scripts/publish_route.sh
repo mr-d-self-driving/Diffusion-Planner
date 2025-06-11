3108f82e984d80b7527a5c67466bd0e90af36227
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-06-11 16:03:26 +0900

    Added publish_route.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

---
+++ ros_scripts/publish_route.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+script_dir=$(readlink -f $(dirname $0))/../ros_scripts
+python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route01_teleport_to_miraikan.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route02_miraikan_to_teleport.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route03_teleport_to_kokusaitenjijo.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route04_kokusaitenjijo_to_miraikan.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route05_miraikan_to_kokusaitenjijo.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route06_kokusaitenjijo_to_teleport.yaml
