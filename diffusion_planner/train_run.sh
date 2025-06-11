36e95217f1ee7e189ead42c0d91fe070c758fc34
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-06-11 14:51:39 +0900

    Push train_run.sh and valid_run.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- diffusion_planner/train_run.sh
+++ diffusion_planner/train_run.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+set -x
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+export NCCL_NVLS_ENABLE=0
+export NCCL_P2P_DISABLE=0
+export NCCL_IB_DISABLE=1
+export NCCL_SOCKET_IFNAME=lo
+export NCCL_DEBUG=INFO
 
 rm -f /tmp/tmp_dist_init
 
 python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone train_predictor.py \
---train_set_list "REPLACE_WITH_TRAIN_SET_LIST_PATH" \
+--train_set_list "/mnt/nvme0/sakoda/nas_copy/private_workspace/diffusion_planner/preprocessed_for_diffusion_planner12/train_set_path_with_psim20250610.json" \
+--valid_set_list "/mnt/nvme0/sakoda/nas_copy/private_workspace/diffusion_planner/preprocessed_for_diffusion_planner12/valid_set_path.json" \
+--use_wandb True \
+--diffusion_model_type "x_start" \
+2>&1 | tee logs/result_$(date +%Y%m%d_%H%M%S).txt
+
+save_dir_name=$(ls ./training_log/diffusion-planner-training/ | tail -n 1)
+
+./valid_run.sh ${save_dir_name}
--- diffusion_planner/valid_run.sh
+++ diffusion_planner/valid_run.sh
@@ -1,18 +1,28 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#!/bin/bash
+set -x
+export CUDA_VISIBLE_DEVICES=0
 
 ###################################
 # User Configuration Section
 ###################################
 
 # Set training data path
-VALID_SET_LIST_PATH="REPLACE_WITH_TRAIN_SET_LIST_PATH"
-MODEL_PATH="REPLACE_WITH_MODEL_PATH"
-ARGS_JSON_PATH="REPLACE_WITH_MODEL_PATH"
-###################################
+DIR_NAME=${1}
+MODEL_DIR="/home/ubuntu/sakoda/Diffusion-Planner/training_log/diffusion-planner-training/$DIR_NAME/"
+VALID_SET_LIST_PATH="/mnt/nvme0/sakoda/nas_copy/private_workspace/diffusion_planner/preprocessed_for_diffusion_planner12/valid_set_teleport_3routes_path.json"
+MODEL_PATH="$MODEL_DIR/latest.pth"
+ARGS_JSON_PATH="$MODEL_DIR/args.json"
+SAVE_DIR=/mnt/nvme0/sakoda/${DIR_NAME}/predictions
 
 rm -f /tmp/tmp_dist_init
 
-python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone valid_predictor.py \
+python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 1 --standalone valid_predictor.py \
 --valid_set_list  $VALID_SET_LIST_PATH \
 --resume_model_path $MODEL_PATH \
 --args_json_path $ARGS_JSON_PATH \
+--save_predictions_dir $SAVE_DIR \
+
+python3 util_scripts/visualize_prediction.py \
+  --predictions_dir $SAVE_DIR \
+  --args_json $ARGS_JSON_PATH \
+  --valid_data_list $VALID_SET_LIST_PATH
