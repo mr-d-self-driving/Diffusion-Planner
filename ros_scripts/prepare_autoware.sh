6539f878a96b47ccea88ddac44e12c99359e36f7
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-07-01 15:37:05 +0900

    Removed pacmod3_msgs
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_scripts/prepare_autoware.sh
+++ ros_scripts/prepare_autoware.sh
@@ -10,7 +10,6 @@ cd autoware/src
 git clone https://github.com/autowarefoundation/autoware_cmake
 git clone https://github.com/autowarefoundation/autoware_msgs
 git clone https://github.com/autowarefoundation/autoware_lanelet2_extension
-git clone https://github.com/astuff/pacmod3_msgs
 ln -s $diffusion_planner_root ./
 cd ../
 
