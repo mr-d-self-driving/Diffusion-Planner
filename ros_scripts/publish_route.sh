62afd9c1ef53d9ec228f9c255efce4a31a2787ec
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-07-14 11:00:41 +0900

    Fixed publish_route.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_scripts/publish_route.sh
+++ ros_scripts/publish_route.sh
@@ -4,7 +4,7 @@ source ./install/setup.bash
 
 set -eux
 script_dir=$(readlink -f $(dirname $0))
-# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route01_teleport_to_miraikan.yaml
+python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route01_teleport_to_miraikan.yaml
 # python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route02_miraikan_to_teleport.yaml
 # python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route03_teleport_to_kokusaitenjijo.yaml
 # python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route/route04_kokusaitenjijo_to_miraikan.yaml
@@ -13,4 +13,4 @@ script_dir=$(readlink -f $(dirname $0))
 
 # python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route_kashiwanoha/00_straight_line.yaml
 # python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route_kashiwanoha/01_taking_curves.yaml
-python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route_kashiwanoha/02_simple_avoidance.yaml
+# python3 $script_dir/publish_initial_and_goal_from_yaml.py $script_dir/route_kashiwanoha/02_simple_avoidance.yaml
