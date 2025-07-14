f29da540cd34158dd78e1d70138b0a75ec997c9d
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-07-14 11:00:08 +0900

    Fixed play.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_scripts/play.sh
+++ ros_scripts/play.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rate=${1:-2.0}
+rate=${1:-1.0}
 
 cd $(dirname $0)/../
 source /opt/ros/humble/setup.bash
 source ./install/setup.bash
-ros2 bag play /home/shintarosakoda/data/misc/20250329_psim_rosbag/rosbag2_2025_03_29-13_12_14 -r $rate
+ros2 bag play /home/shintarosakoda/data/nas_copy/tieriv_dataset/driving_dataset/bag/2025-04-16/10-47-50 -r $rate --remap /planning/scenario_planning/trajectory:=/unused
